923790db710af59b108f3099a2f2322afc39edf8
yzrsng <43310551+yzrsng@users.noreply.github.com>
2019-09-16 09:17:29 +0900

    Update global-white-for-yuzu.js

--- global-white-for-yuzu.js
+++ global-white-for-yuzu.js
@@ -3,7 +3,7 @@
 // @namespace Violentmonkey Scripts
 // @author yzrsng
 // @description Userscript for Invert Rendering by Yuzu Browser.
-// @version 0.1
+// @version 0.2
 // @include *
 // @match *://*/*
 // @grant none
@@ -11,10 +11,19 @@
 
 var css = document.createElement('style');
 css.type = "text/css";
-css.innerHTML = 'html>body *{background-color:#ffffff80 !important;text-shadow:1px 0px 0 #80808020, 0px -1px 0 #80808020, 1px -1px 0 #80808020}html>body{color:initial !important;background-color:initial !important}button,button *,p,p *,h1,h1 *,h2,h2 *,h3,h3 *,h4,h4 *,h5,h5 *,h6,h6 *,input,input *,label,label *,li,li *,thead,thead *,tbody,tbody *,tr,tr *,th,th *,td,td *,svg,svg *,span,span[class],div,div[class],[class*="text"]{color:#102040e0 !important;background-color:#ffffff40 !important;border-color:#00000020 !important}body blockquote,body blockquote>*,body cite,body cite>*{color:#000000c0 !important;background-color:#ffffff20 !important}body a,body a *,body a[href],body a[href] *,body a[class],body a[class] *,body a[class][href],body a[class][href] *{color:blue !important;background-color:initial !important}body a:visited,body a:visited *,body a[href]:visited,body a[href]:visited *,body a[class]:visited,body a[class]:visited *,body a[class][href]:visited,body a[class][href]:visited *{color:purple !important}';
+css.innerHTML = 'html>body *{background-color:#ffffff80 !important;text-shadow:1px 0px 0 #80808020, 0px -1px 0 #80808020, 1px -1px 0 #80808020}html>body{color:initial !important;background-color:initial !important}button,button *,p,p *,h1,h1 *,h2,h2 *,h3,h3 *,h4,h4 *,h5,h5 *,h6,h6 *,input,input *,label,label *,li,li *,thead,thead *,tbody,tbody *,tr,tr *,th,th *,td,td *,svg,svg *,span,span[class],div,div[class],[class*="text"]{color:#102040e0 !important;background-color:#ffffff40 !important;border-color:#00000020 !important}body blockquote,body blockquote>*,body cite,body cite>*{color:#000000c0 !important;background-color:#ffffff20 !important}body a,body a *,body a[href],body a[href] *,body a[class],body a[class] *,body a[class][href],body a[class][href] *{color:blue !important;background-color:initial !important}body a:visited,body a:visited *,body a[href]:visited,body a[href]:visited *,body a[class]:visited,body a[class]:visited *,body a[class][href]:visited,body a[class][href]:visited *{color:purple !important}*[style*=".jpg"],*[style*=".png"],*[style*=".webp"]{-webkit-filter:invert(100%)}';
 document.getElementsByTagName('head')[0].appendChild(css);
 document.getElementsByTagName("body")[0].setAttribute("bgcolor", "white");
 document.getElementsByTagName("body")[0].setAttribute("text", "black");
 document.getElementsByTagName("body")[0].setAttribute("alink", "blue");
 document.getElementsByTagName("body")[0].setAttribute("link", "blue");
 document.getElementsByTagName("body")[0].setAttribute("vlink", "blue");
+
+var myElements = document.querySelectorAll('*');
+var siteStyle;
+for (var i = 0; i < myElements.length; i++) {
+  siteStyle = window.getComputedStyle(myElements[i]);
+  if (siteStyle.getPropertyValue('background-image').match(/url\(/)) {
+    myElements[i].style.setProperty("-webkit-filter","invert(100%)","");
+  }
+}
