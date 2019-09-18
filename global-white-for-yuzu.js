0b6602f83d2ffab5712a98e8bc247633e12022b3
yzrsng <43310551+yzrsng@users.noreply.github.com>
2019-09-18 21:36:25 +0900

    Update global-white-for-yuzu.js

--- global-white-for-yuzu.js
+++ global-white-for-yuzu.js
@@ -11,10 +11,23 @@
 
 var css = document.createElement('style');
 css.type = "text/css";
-css.innerHTML = 'html>body *{background-color:#ffffff80 !important;text-shadow:1px 0px 0 #80808020, 0px -1px 0 #80808020, 1px -1px 0 #80808020}html>body{color:initial !important;background-color:initial !important}button,button *,p,p *,h1,h1 *,h2,h2 *,h3,h3 *,h4,h4 *,h5,h5 *,h6,h6 *,input,input *,label,label *,li,li *,thead,thead *,tbody,tbody *,tr,tr *,th,th *,td,td *,svg,svg *,span,span[class],div,div[class],[class*="text"]{color:#102040e0 !important;background-color:#ffffff40 !important;border-color:#00000020 !important}body blockquote,body blockquote>*,body cite,body cite>*{color:#000000c0 !important;background-color:#ffffff20 !important}body a,body a *,body a[href],body a[href] *,body a[class],body a[class] *,body a[class][href],body a[class][href] *{color:#ff0000 !important;background-color:initial !important}body a:visited,body a:visited *,body a[href]:visited,body a[href]:visited *,body a[class]:visited,body a[class]:visited *,body a[class][href]:visited,body a[class][href]:visited *{color:#ff0080 !important}img,video,canvas,iframe[src*="embed"],iframe[data-src*="embed"],*[style*="background-image"]:empty,*[style*=".jpeg"]:empty,*[style*=".jpg"]:empty,*[style*=".gif"]:empty,*[style*=".png"]:empty,*[style*=".webp"]:empty{-webkit-filter:invert(100%)}';
+css.innerHTML = 'html>body *{background-color:#ffffff80 !important;text-shadow:1px 0px 0 #80808020, 0px -1px 0 #80808020, 1px -1px 0 #80808020}html>body{color:initial !important;background-color:initial !important}button,button *,p,p *,h1,h1 *,h2,h2 *,h3,h3 *,h4,h4 *,h5,h5 *,h6,h6 *,input,input *,label,label *,li,li *,thead,thead *,tbody,tbody *,tr,tr *,th,th *,td,td *,svg,svg *,span,span[class],div,div[class],[class*="text"]{color:#102040e0 !important;background-color:#ffffff40 !important;border-color:#00000020 !important}body blockquote,body blockquote>*,body cite,body cite>*{color:#000000c0 !important;background-color:#ffffff20 !important}body a,body a *,body a[href],body a[href] *,body a[class],body a[class] *,body a[class][href],body a[class][href] *{color:#ff0000 !important;background-color:initial !important}body a:visited,body a:visited *,body a[href]:visited,body a[href]:visited *,body a[class]:visited,body a[class]:visited *,body a[class][href]:visited,body a[class][href]:visited *{color:#ff0080 !important}iframe[src*="embed"],iframe[data-src*="embed"],img,video,canvas,*[style*="background-image"]:empty,*[style*=".jpeg"]:empty,*[style*=".jpg"]:empty,*[style*=".gif"]:empty,*[style*=".png"]:empty,*[style*=".webp"]:empty{-webkit-filter:invert(100%)}';
 document.getElementsByTagName('head')[0].appendChild(css);
 document.getElementsByTagName("body")[0].setAttribute("bgcolor", "white");
 document.getElementsByTagName("body")[0].setAttribute("text", "black");
 document.getElementsByTagName("body")[0].setAttribute("alink", "red");
 document.getElementsByTagName("body")[0].setAttribute("link", "red");
 document.getElementsByTagName("body")[0].setAttribute("vlink", "red");
+
+var twElements = document.querySelectorAll('body *');
+var ssStyle;
+for (let i = 0; i < twElements.length; i++) {
+  ssStyle = window.getComputedStyle(twElements[i]);
+  // 多重反転を防ぐために小要素を持たない要素のみ
+  if (twElements[i].childElementCount === 0) {
+    // 背景画像を設定している要素のみを反転
+    if (ssStyle.getPropertyValue('background-image').match(/url\(/)) {
+      twElements[i].style.setProperty("-webkit-filter","invert(100%)","");
+    }
+  }
+}
