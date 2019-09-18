d3798a28e426fd5bb7141db049c8dea117f9df1e
yzrsng <43310551+yzrsng@users.noreply.github.com>
2019-09-19 07:22:14 +0900

    Add files via upload

---
+++ invert-all-image.js
@@ -0,0 +1,57 @@
+// ==UserScript==
+// @name Invert All Image Script
+// @namespace Violentmonkey Scripts
+// @author yzrsng
+// @description Userscript for Invert Rendering by Yuzu Browser.
+// @version 0.1
+// @include *
+// @match *://*/*
+// @grant none
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+  var css = document.createElement('style');
+  css.type = "text/css";
+  css.innerHTML = 'iframe[src*="embed"]:not(.invertedChildren-yz),iframe[data-src*="embed"]:not(.invertedChildren-yz),img:not(.invertedChildren-yz),video:not(.invertedChildren-yz),canvas:not(.invertedChildren-yz),.invertedRoot-yz{-webkit-filter:invert(100%)}';
+  document.getElementsByTagName('head')[0].appendChild(css);
+  
+  // function printElms(elms) {
+  //   console.log(elms);
+  //   console.log(elms.length);
+  // }
+
+  function markChildElms(elms) {
+    for (let i = 0; i < elms.length; i++) {
+      elms[i].classList.add('patrolled-yz');
+      elms[i].classList.add('invertedChildren-yz');
+      markChildElms(elms[i].children);
+    }
+  }
+
+  function invertRootElms(elms) {
+    // console.log(elms.length);
+    let ssStyle;
+    for (let i = 0; i < elms.length; i++) {
+      if (elms[i].classList.contains('patrolled-yz')) {
+        continue;
+      }
+      elms[i].classList.add('patrolled-yz');
+      ssStyle = window.getComputedStyle(elms[i]);
+      if (elms[i].tagName === 'IMG' || elms[i].tagName === 'VIDEO' || elms[i].tagName === 'CANVAS' || ssStyle.getPropertyValue('background-image').match(/url\(/)) {
+        elms[i].classList.add('invertedRoot-yz');
+        elms[i].style.setProperty("-webkit-filter","invert(100%)","");
+        markChildElms(elms[i].children);
+      }
+    }
+  }
+  
+  let myElms = document.getElementsByTagName('*');
+
+  // invertRootElms(myElms);
+
+  var id = setInterval(function(){
+    invertRootElms(myElms);
+  }, 500);
+})();
