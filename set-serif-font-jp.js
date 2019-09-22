3c77bfdd24d28715454f08185fe909d1dcbdb3c8
yzrsng <43310551+yzrsng@users.noreply.github.com>
2019-09-22 14:07:31 +0900

    Update set-serif-font-jp.js

--- set-serif-font-jp.js
+++ set-serif-font-jp.js
@@ -12,15 +12,15 @@
 (function() {
   'use strict';
 
-  const myLink = document.createElement('link');
-  myLink.id = 'set_serif_font_link';
-  myLink.setAttribute("rel", "stylesheet");
-  myLink.setAttribute("href", "https://fonts.googleapis.com/css?family=Noto+Serif+JP|Sawarabi+Mincho&display=swap");
-  document.getElementsByTagName('head')[0].appendChild(myLink);
-
   const myCss = document.createElement('style');
   myCss.type = "text/css";
   myCss.id = 'set_serif_font_style';
   myCss.insertAdjacentHTML('beforeend', '*{font-family:"Noto Serif JP","Sawarabi Mincho",sans-serif}');
   document.getElementsByTagName('head')[0].appendChild(myCss);
+
+  const myLink = document.createElement('link');
+  myLink.id = 'set_serif_font_link';
+  myLink.setAttribute("rel", "stylesheet");
+  myLink.setAttribute("href", "https://fonts.googleapis.com/css?family=Noto+Serif+JP|Sawarabi+Mincho&display=swap");
+  document.getElementsByTagName('head')[0].appendChild(myLink);
 })();
