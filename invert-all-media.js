976849c6141ee297689855b037f3bf943d6adbb3
yzrsng <43310551+yzrsng@users.noreply.github.com>
2019-09-22 10:39:35 +0900

    Update invert-all-media.js

--- invert-all-media.js
+++ invert-all-media.js
@@ -14,82 +14,89 @@
   
   // remove build in filter for Invert mode on Yuzu Browser
   const yuzubrowserInvertModeStyle = document.getElementById('yuzubrowser_invert_mode');
-  console.log(yuzubrowserInvertModeStyle);
-  console.log(typeof yuzubrowserInvertModeStyle);
   if (yuzubrowserInvertModeStyle !== null) {
     yuzubrowserInvertModeStyle.parentNode.removeChild(yuzubrowserInvertModeStyle);
   }
 
   const patrolledClass = "invertPatrolledElement-yz";
-  const InvertClassRoot = "invertedRootElement-yz";
-  const InvertClassChild = "invertedChildElement-yz";
+  const invertClassRoot = "invertedRootElement-yz";
+  const invertClassChild = "invertedChildElement-yz";
   const myCss = document.createElement('style');
   myCss.type = "text/css";
   myCss.id = 'dynamic_invert_media_filter';
-  myCss.insertAdjacentHTML('beforeend', 'iframe[src*="embed"]:not(.' + InvertClassChild + '),');
-  myCss.insertAdjacentHTML('beforeend', 'iframe[data-src*="embed"]:not(.' + InvertClassChild + '),');
-  myCss.insertAdjacentHTML('beforeend', 'img:not(.' + InvertClassChild + '),');
-  myCss.insertAdjacentHTML('beforeend', 'video:not(.' + InvertClassChild + '),');
-  myCss.insertAdjacentHTML('beforeend', 'canvas:not(.' + InvertClassChild + '),');
-  myCss.insertAdjacentHTML('beforeend', '.' + InvertClassRoot + '{filter:invert(100%)}');
+  myCss.insertAdjacentHTML('beforeend', 'iframe[src*="embed"]:not(.' + invertClassChild + '),');
+  myCss.insertAdjacentHTML('beforeend', 'iframe[data-src*="embed"]:not(.' + invertClassChild + '),');
+  myCss.insertAdjacentHTML('beforeend', 'img:not(.' + invertClassChild + '),');
+  myCss.insertAdjacentHTML('beforeend', 'embed:not(.' + invertClassChild + '),');
+  myCss.insertAdjacentHTML('beforeend', 'svg:not(.' + invertClassChild + '),');
+  myCss.insertAdjacentHTML('beforeend', 'video:not(.' + invertClassChild + '),');
+  myCss.insertAdjacentHTML('beforeend', 'canvas:not(.' + invertClassChild + '),');
+  myCss.insertAdjacentHTML('beforeend', '.' + invertClassRoot + '{filter:invert(100%)}');
   document.getElementsByTagName('head')[0].appendChild(myCss);
+
+  const invertExistingFilterClass = "invertedFilterElement-yz-";
+  let invertExistingFilterCount = 1;
+
+  const judgeBgColorClassWhite = "judgeBgColor-yz-white";
+  const judgeBgColorClassBlack = "judgeBgColor-yz-Black";
+  myCss.insertAdjacentHTML('beforeend', '.' + judgeBgColorClassWhite + '{background-color:rgba(255, 255, 255, 0.75) !important}');
+  myCss.insertAdjacentHTML('beforeend', '.' + judgeBgColorClassBlack + '{background-color:rgba(0, 0, 0, 0.75) !important}');
+
+  const grantedBgColorClass = "grantBgColor-yz-";
+  let grantBgColorCount = 1;
+  myCss.insertAdjacentHTML('beforeend', '.' + grantedBgColorClass + "0" + '{background-color:white !important}');
   
   // function printElms(elms) {
   //   console.log(elms);
   //   console.log(elms.length);
   // }
 
-  function returnJudgeBgColor(elm) {
-    const elmStyle = window.getComputedStyle(elm);
-    const elmColor = elmStyle.getPropertyValue("color");
-    const posNumber = elmColor.indexOf("(");
-    const elmClrValue = elmColor.substring(posNumber+1,elmColor.length-1);
-    const clrArray = elmClrValue.split(', ');
-    for (let i = 0; i < clrArray.length; i++) {
-      clrArray[i] = parseInt(clrArray[i], 10);
-    }
-    const judgePoint = clrArray[0]*9 + clrArray[1]*13 + clrArray[2]*8;
-    if (judgePoint < 3830) {
-      return "rgba(255, 255, 255, 0.75)";
-    }
-    return "rgba(0, 0, 0, 0.75)";
-  }
+  // function judgeBgColor(elm) {
+  //   const elmStyle = window.getComputedStyle(elm);
+  //   const elmColor = elmStyle.getPropertyValue("color");
+  //   const posNumber = elmColor.indexOf("(");
+  //   const elmClrValue = elmColor.substring(posNumber+1,elmColor.length-1);
+  //   const clrArray = elmClrValue.split(', ');
+  //   for (let i = 0; i < clrArray.length; i++) {
+  //     clrArray[i] = parseInt(clrArray[i], 10);
+  //   }
+  //   const judgePoint = clrArray[0]*9 + clrArray[1]*13 + clrArray[2]*8;
+  //   if (judgePoint < 3830) {
+  //     elm.classList.add(judgeBgColorClassWhite);
+  //   } else {
+  //     elm.classList.add(judgeBgColorClassWhite);
+  //   }
+  // }
 
-  let grantBgColorCount = 0;
   function returnParentsBgColor(elm) {
-    console.log("grantBgColorCount: "+ ++grantBgColorCount);
-    myCss.insertAdjacentHTML('beforeend', "");
-    const elmStyle = window.getComputedStyle(elm);
-    const elmBgColor = elmStyle.getPropertyValue("background-color");
-    if (elmBgColor === "rgba(0, 0, 0, 0)") {
-      if (elm.tagName === "HTML") {
+    const parentElmStyle = window.getComputedStyle(elm.parentNode);
+    const parentElmBgColor = parentElmStyle.getPropertyValue("background-color");
+    if (parentElmBgColor === "rgba(0, 0, 0, 0)") {
+      if (elm.parentNode.tagName === "HTML") {
         return "rgb(0, 0, 0)";
       }
       return returnParentsBgColor(elm.parentNode);
     }
-    return elmBgColor;
+    return parentElmBgColor;
   }
 
   function markChildElms(elms) {
     for (let i = 0; i < elms.length; i++) {
       elms[i].classList.add(patrolledClass);
-      elms[i].classList.add(InvertClassChild);
+      elms[i].classList.add(invertClassChild);
       markChildElms(elms[i].children);
     }
   }
 
   function invertRootElms(elms) {
-    if (window.getComputedStyle(elms[0]).getPropertyValue('background-color') === "rgba(0, 0, 0, 0)") {
-      elms[0].style.setProperty("background-color", "rgb(255, 255, 255)", "");
-    }
     for (let i = 0; i < elms.length; i++) {
       if (elms[i].classList.contains(patrolledClass)) {
         continue;
       }
       elms[i].classList.add(patrolledClass);
       if (elms[i].tagName !== "HTML") {
-        if (elms[i].parentNode.classList.contains(InvertClassChild)) {
-          elms[i].classList.add(InvertClassChild);
+        if (elms[i].parentNode.classList.contains(invertClassChild)) {
+          elms[i].classList.add(invertClassChild);
           markChildElms(elms[i].children);
           continue;
         }
@@ -97,17 +104,27 @@
       const ssStyle = window.getComputedStyle(elms[i]);
       const ssStyleBefore = window.getComputedStyle(elms[i], '::before');
       const ssStyleAfter = window.getComputedStyle(elms[i], '::after');
-      if (elms[i].tagName === 'IMG' || elms[i].tagName === 'VIDEO' || elms[i].tagName === 'CANVAS' || ssStyle.getPropertyValue('background-image').match(/url\(/) || ssStyleBefore.getPropertyValue('background-image').match(/url\(/) || ssStyleAfter.getPropertyValue('background-image').match(/url\(/)) {
+      if (ssStyle.getPropertyValue('background-image').match(/url\(/) || ssStyleBefore.getPropertyValue('background-image').match(/url\(/) || ssStyleAfter.getPropertyValue('background-image').match(/url\(/) || elms[i].tagName === 'IMG' || elms[i].tagName === 'EMBED' || elms[i].tagName === 'SVG' || elms[i].tagName === 'VIDEO' || elms[i].tagName === 'CANVAS') {
         // if (ssStyle.getPropertyValue('background-repeat').match(/-/) || ssStyle.getPropertyValue('background-repeat').match(/space/)) {
-        //   // elms[i].style.setProperty("background-color", returnJudgeBgColor(elms[i]), "");
-        //   elms[i].style.setProperty("background-color", returnParentsBgColor(elms[i]), "");
+        //   // judgeBgColor(elms[i]);
+        //   elms[i].classList.add(grantedBgColorClass + grantBgColorCount);
+        //   myCss.insertAdjacentHTML('beforeend', "." + grantedBgColorClass + grantBgColorCount + "{background-color:" + returnParentsBgColor(elms[i]) + " !important}");
+        //   grantBgColorCount++;
         // }
         if (ssStyle.getPropertyValue('background-color') === "rgba(0, 0, 0, 0)" && elms[i].childNodes.length !== 0) {
-          // elms[i].style.setProperty("background-color", returnJudgeBgColor(elms[i]), "");
-          elms[i].style.setProperty("background-color", returnParentsBgColor(elms[i]), "");
+          // judgeBgColor(elms[i]);
+          elms[i].classList.add(grantedBgColorClass + grantBgColorCount);
+          myCss.insertAdjacentHTML('beforeend', "." + grantedBgColorClass + grantBgColorCount + "{background-color:" + returnParentsBgColor(elms[i]) + " !important}");
+          grantBgColorCount++;
         }
-        elms[i].classList.add(InvertClassRoot);
-        elms[i].style.setProperty("filter","invert(100%)","");
+        const filterValue = ssStyle.getPropertyValue('filter');
+        if (filterValue !== "none") {
+          elms[i].classList.add(invertExistingFilterClass + invertExistingFilterCount);
+          myCss.insertAdjacentHTML('beforeend', "." + invertExistingFilterClass + invertExistingFilterCount + "{filter:" + filterValue + " invert(1)" + "}");
+          invertExistingFilterCount++;
+        } else {
+          elms[i].classList.add(invertClassRoot);
+        }
         if (elms[i].children.length === 0) {
           continue;
         }
@@ -118,9 +135,13 @@
   
   const myElms = document.getElementsByTagName('*');
 
+  if (window.getComputedStyle(myElms[0]).getPropertyValue('background-color') === "rgba(0, 0, 0, 0)") {
+    myCss.insertAdjacentHTML('beforeend', "html{background-color:white}");
+  }
+
   // invertRootElms(myElms);
 
   const id = setInterval(function(){
     invertRootElms(myElms);
-  }, 1000);
+  }, 2000);
 })();
