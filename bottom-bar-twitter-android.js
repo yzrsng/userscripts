a72d55ae844c7d22f583c0e9e8b9813760868d23
yzrsng <43310551+yzrsng@users.noreply.github.com>
2019-09-22 23:09:44 +0900

    Update bottom-bar-twitter-android.js

--- bottom-bar-twitter-android.js
+++ bottom-bar-twitter-android.js
@@ -4,12 +4,15 @@
 // @author euser
 // @description Userscript on twitter for your Android phone.
 // @version 0.1
-// @include *://mobile.twitter.com/*
-// @match *://mobile.twitter.com/*
+// @include http://mobile.twitter.com/*
+// @include https://mobile.twitter.com/*
+// @match http://mobile.twitter.com/*
+// @match https://mobile.twitter.com/*
 // @grant none
 // ==/UserScript==
 
 var cssB = document.createElement('style');
 cssB.type = "text/css";
+cssB.id = "mobile-twitter_bottom_bar_style";
 cssB.innerHTML = 'header>div{height:49px !important}header>div:nth-of-type(2)>div:first-of-type>div>div>div>div{border-bottom-width:1px !important;border-bottom-color:#70809040 !important}header>div>div>div>nav{position:fixed !important;bottom:0 !important}header>div>div>div>nav>a{border-bottom-color:transparent!important}aside[role="complementary"]>div{bottom:67px}div#react-root>div[data-reactroot]>div>div>div,div#react-root>div[data-reactroot]>div>div>div>aside[role="complementary"],div#react-root>div[data-reactroot]>div>div>div>aside[role="complementary"]>div,div#react-root>div[data-reactroot]>div>div>div>aside[role="complementary"]>div>div[role="button"]>div[dir="auto"]{background-color:transparent !important}';
 document.getElementsByTagName('head')[0].appendChild(cssB);
