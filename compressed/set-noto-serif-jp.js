6ee5e8a364a9f30d10497ca092cbb2eb4627245f
yzrsng <43310551+yzrsng@users.noreply.github.com>
2019-09-25 23:33:43 +0900

    Add files via upload

---
+++ compressed/set-noto-serif-jp.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name set Noto Serif JP
+// @namespace Violentmonkey Scripts
+// @author yzrsng
+// @description Userscript for Serif Font.
+// @version 0.1
+// @include http://*
+// @include https://*
+// @match http://*
+// @match https://*
+// @grant none
+// ==/UserScript==
+
+(function(){"use strict";const n=document.createElement("style");n.type="text/css";n.id="set_serif_font_style";n.insertAdjacentHTML("beforeend","html{font-family:'Noto Serif JP',serif;}");document.getElementsByTagName("head")[0].appendChild(n);const t=document.createElement("link");t.id="set_serif_font_link";t.setAttribute("rel","stylesheet");t.setAttribute("href","https://fonts.googleapis.com/css?family=Noto+Serif+JP&display=swap&subset=japanese");document.getElementsByTagName("head")[0].appendChild(t)})()
