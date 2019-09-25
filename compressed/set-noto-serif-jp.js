bdaf332aaad43da8f5081ffca3d20cb9bc5aafa5
yzrsng <43310551+yzrsng@users.noreply.github.com>
2019-09-26 00:30:45 +0900

    Update set-noto-serif-jp.js

--- compressed/set-noto-serif-jp.js
+++ compressed/set-noto-serif-jp.js
@@ -11,4 +11,5 @@
 // @grant none
 // ==/UserScript==
 
-(function(){"use strict";const n=document.createElement("style");n.type="text/css";n.id="set_serif_font_style";n.insertAdjacentHTML("beforeend","html{font-family:'Noto Serif JP',serif;}");document.getElementsByTagName("head")[0].appendChild(n);const t=document.createElement("link");t.id="set_serif_font_link";t.setAttribute("rel","stylesheet");t.setAttribute("href","https://fonts.googleapis.com/css?family=Noto+Serif+JP&display=swap&subset=japanese");document.getElementsByTagName("head")[0].appendChild(t)})()
+(function(){"use strict";const n=document.createElement("style");n.type="text/css";n.id="set_serif_font_style";n.insertAdjacentHTML("beforeend","
+(function(){"use strict";const n=document.createElement("style");n.type="text/css";n.id="set_serif_font_style";n.insertAdjacentHTML("beforeend","*:not(pre):not(span):not(code):not(samp){font-family:'Noto Serif JP',serif;}");document.getElementsByTagName("head")[0].appendChild(n);const t=document.createElement("link");t.id="set_serif_font_link";t.setAttribute("rel","stylesheet");t.setAttribute("href","https://fonts.googleapis.com/css?family=Noto+Serif+JP&display=swap&subset=japanese");document.getElementsByTagName("head")[0].appendChild(t)})()
