64f1b9f9e29a7da9f87235394c743344d53642c0
yzrsng <43310551+yzrsng@users.noreply.github.com>
2019-09-25 23:43:27 +0900

    Update set-sawarabi-mincho.js

--- compressed/set-sawarabi-mincho.js
+++ compressed/set-sawarabi-mincho.js
@@ -11,4 +11,4 @@
 // @grant none
 // ==/UserScript==
 
-(function(){"use strict";const n=document.createElement("style");n.type="text/css";n.id="set_serif_font_style";n.insertAdjacentHTML("beforeend","html{font-family:'Sawarabi Mincho',serif;}");document.getElementsByTagName("head")[0].appendChild(n);const t=document.createElement("link");t.id="set_serif_font_link";t.setAttribute("rel","stylesheet");t.setAttribute("href","https://fonts.googleapis.com/css?family=Sawarabi+Mincho&display=swap&subset=japanese");document.getElementsByTagName("head")[0].appendChild(t)})()
+(function(){"use strict";const n=document.createElement("style");n.type="text/css";n.id="set_serif_font_style";n.insertAdjacentHTML("beforeend","*:not(pre):not(span):not(code):not(samp){font-family:'Sawarabi Mincho',serif;}");document.getElementsByTagName("head")[0].appendChild(n);const t=document.createElement("link");t.id="set_serif_font_link";t.setAttribute("rel","stylesheet");t.setAttribute("href","https://fonts.googleapis.com/css?family=Sawarabi+Mincho&display=swap&subset=japanese");document.getElementsByTagName("head")[0].appendChild(t)})()
